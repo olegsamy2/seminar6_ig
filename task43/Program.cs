fb91349c47c4e000e75a39bc368d32820233b032
OlegSam2 <doorsmania@mail.ru>
2022-08-28 22:26:05 +0300

    intersections of two straight lines

--- task43/Program.cs
+++ task43/Program.cs
@@ -2,5 +2,31 @@
 // заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
 // b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
 
+Console.WriteLine("Введите число b1");
+double b1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите число k1");
+double k1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите число b2");
+double b2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите число k2");
+double k2 = Convert.ToDouble(Console.ReadLine());
+
+if (k1 == k2 && b1 == b2)                   // одна и та же прямая
+{
+
+    Console.WriteLine("Прямые совпадают");
+}
+if (k1 == k2 && b1 != b2)                  // условие параллельных прямых  - угловые коэффициенты равны
+{
+    Console.WriteLine("Параллельные прямые не пересекаются");
+}
+if (k1 != k2)
+{
+    double x = (b2 - b1) / (k1 - k2);
+    double y = (-b2 * k1 + b1 * k2) / -(k1 - k2);
+
+    Console.WriteLine($"Координата точки пересечения двух прямых x = {x}, y = {y}");
+}
+
 
 
